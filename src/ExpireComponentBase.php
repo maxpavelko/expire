e8b5fb4eb3611002c778f3c6fe88f780889cf71f
Vadym Myrgorod <vm386@georgetown.edu>
2015-10-30 18:14:41 +0000

    Add base class for expire component

---
+++ src/ExpireComponentBase.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vm386
+ * Date: 10/30/15
+ * Time: 2:06 PM
+ */
+
+namespace Drupal\expire;
+
+use Drupal\Core\Form\FormStateInterface;
+
+class ExpireComponentBase extends PluginSettingsBase implements ExpireComptonentInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function settingsForm(array $form, FormStateInterface $form_state) {
+    return array();
+  }
+
+  public function expire($object, $action, $skip_action_check = FALSE) {}
+}
