9b85a62dfcc1a09f51629bd02bf0d5eb5e671e5f
Vadym Myrgorod <vm386@georgetown.edu>
2015-10-30 18:03:15 +0000

    Add Component manager, anotation and interface

---
+++ src/Annotation/ExpireComponent.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\expire\Annotation\ExpireComponent.
+ */
+
+namespace Drupal\expire\Annotation;
+
+use Drupal\Component\Annotation\Plugin;
+
+/**
+ * Defines a Expire Component annotation object.
+ *
+ * Plugin Namespace: Plugin\expire\ExpireComponent
+ *
+ * @see \Drupal\expire\ExpireComponentManager
+ * @see plugin_api
+ *
+ * @Annotation
+ */
+class ExpireComponent extends Plugin {
+
+    /**
+     * The plugin ID.
+     *
+     * @var string
+     */
+    public $id;
+
+    /**
+     * The name of the component.
+     *
+     * @var \Drupal\Core\Annotation\Translation
+     *
+     * @ingroup plugin_translatable
+     */
+    public $name;
+}
---
+++ src/ExpireComptonentInterface.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: vm386
+ * Date: 10/30/15
+ * Time: 1:39 PM
+ */
+
+namespace Drupal\expire;
+
+
+interface ExpireComptonentInterface extends PluginSettingsInterface {
+    /**
+     * Returns a form to configure settings for the expire component.
+     *
+     * @param array $form
+     *   The form where the settings form is being included in.
+     * @param \Drupal\Core\Form\FormStateInterface $form_state
+     *   The current state of the form.
+     *
+     * @return array
+     *   The form elements for the formatter settings.
+     */
+    public function settingsForm(array $form, FormStateInterface $form_state);
+
+    /**
+     * Executes expiration actions for node.
+     *
+     * @param object
+     *   Component object.
+     *
+     * @param $action
+     *   Action that has been executed.
+     *
+     * @param $skip_action_check
+     *   Shows whether should we check executed action or just expire node.
+     */
+    public function expire($object, $action, $skip_action_check = FALSE);
+}
